c11f2f47d8768d5e96e1b707a9f90cba1394b8e2
sumit245 <sumitranjan245@gmail.com>
2022-11-01 01:17:01 +0530

    Message

--- src/scenes/Components/home/mealselector/MealSelector.js
+++ src/scenes/Components/home/mealselector/MealSelector.js
@@ -5,17 +5,17 @@ import { Switch } from 'react-native-paper'
 
 export default function MealSelector() {
     const [isLunch, setIsLunch] = useState(false);
-    const [isDelivery, setIsDelivery] = useState(true);
+    const [isDelivery, setIsDelivery] = useState(false);
     const onToggleMeal = () => setIsLunch(!isLunch);
     const onTogglePickup = () => setIsDelivery(!isDelivery);
     return (
         <View style={styles.header}>
             <View style={{ flexDirection: 'row', alignItems: "center" }}>
-                <Text>Lunch</Text>
+                <Text>{isLunch ? "Dinner" : "Lunch"}</Text>
                 <Switch value={isLunch} onValueChange={onToggleMeal} />
             </View>
             <View style={{ flexDirection: 'row', alignItems: "center" }}>
-                <Text>Chef Pickup </Text>
+                <Text>{isDelivery ? "Doorstep Delivery" : "Chef Pickup"}</Text>
                 <Switch value={isDelivery} onValueChange={onTogglePickup} />
             </View>
 
