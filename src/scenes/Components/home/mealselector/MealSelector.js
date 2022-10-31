6f7593084feb6d1f6e35d3996928eefc8931e5a1
sumit245 <sumitranjan245@gmail.com>
2022-11-01 01:48:08 +0530

    message

--- src/scenes/Components/home/mealselector/MealSelector.js
+++ src/scenes/Components/home/mealselector/MealSelector.js
@@ -9,7 +9,7 @@ export default function MealSelector() {
     const onToggleMeal = () => setIsLunch(!isLunch);
     const onTogglePickup = () => setIsDelivery(!isDelivery);
     return (
-        <View style={styles.header}>
+        <View style={[styles.header, { height: 36 }]}>
             <View style={{ flexDirection: 'row', alignItems: "center" }}>
                 <Text style={styles.title}>{isLunch ? "Dinner" : "Lunch"}</Text>
                 <Switch value={isLunch} onValueChange={onToggleMeal} style={styles.selectorSwitch} color="#ff6600" />
