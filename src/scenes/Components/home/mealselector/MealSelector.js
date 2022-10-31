a3a7235c321e737684229569f2f06f822765a3e5
sumit245 <sumitranjan245@gmail.com>
2022-11-01 01:38:26 +0530

    Message

--- src/scenes/Components/home/mealselector/MealSelector.js
+++ src/scenes/Components/home/mealselector/MealSelector.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { styles } from '../../../styles/HomeStyle'
 import { Switch } from 'react-native-paper'
@@ -12,7 +12,7 @@ export default function MealSelector() {
         <View style={styles.header}>
             <View style={{ flexDirection: 'row', alignItems: "center" }}>
                 <Text style={styles.title}>{isLunch ? "Dinner" : "Lunch"}</Text>
-                <Switch value={isLunch} onValueChange={onToggleMeal} style={{ height: 18 }} color="#ff6600" />
+                <Switch value={isLunch} onValueChange={onToggleMeal} style={{ transform: Platform.OS === "ios" ? [{ scaleX: 1 }, { scaleY: .5 }] : [{ scaleX: 1 }, {scaleY: 1 }] }} color="#ff6600" />
             </View>
             <View style={{ flexDirection: 'row', alignItems: "center" }}>
                 <Text style={styles.title}>{isDelivery ? " Delivery" : "Chef Pickup"}</Text>
