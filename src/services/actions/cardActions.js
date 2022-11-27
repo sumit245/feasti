7dc2a49b48b12720f0bb75a11ec4c63bb2d3b10d
sumit245 <sumitranjan245@gmail.com>
2022-11-28 02:47:59 +0530

    message

--- src/services/actions/cardActions.js
+++ src/services/actions/cardActions.js
@@ -5,7 +5,7 @@ import { ADD_CARD_URL } from "../EndPoints";
 export const addCard = (cardToAdd) => async (dispatch) => {
     const user = await AsyncStorageLib.getItem('user')
     const { _id } = JSON.parse(user)
-    const response = await axios.put(`${ADD_CARD_URL}${_id}`, cardToAdd)
+    const response = await axios.put(`${ADD_CARD_URL}${_id}`, { card: cardToAdd })
     const { data } = response
     console.log('====================================');
     console.log(data);
