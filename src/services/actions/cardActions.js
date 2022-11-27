dcd43a2c5160c359c2bf31ead29e3c7e44fd8d15
sumit245 <sumitranjan245@gmail.com>
2022-11-28 02:39:44 +0530

    message

--- src/services/actions/cardActions.js
+++ src/services/actions/cardActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorageLib from "@react-native-async-storage/async-storage";
 
 export const addCard = (cardToAdd) => async (dispatch) => {
-    const { user } = await AsyncStorageLib.getItem('user')
+    const user = await AsyncStorageLib.getItem('user')
     const { _id } = JSON.parse(user)
     console.log('====================================');
     console.log(_id, cardToAdd);
