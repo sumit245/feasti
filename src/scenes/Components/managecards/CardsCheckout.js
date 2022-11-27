e0a880a2475a6c569a597473d597c8c2f262afe5
sumit245 <sumitranjan245@gmail.com>
2022-11-28 01:15:09 +0530

    Manage Cards'

--- src/scenes/Components/managecards/CardsCheckout.js
+++ src/scenes/Components/managecards/CardsCheckout.js
@@ -1,14 +1,28 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { styles } from '../../styles/HomeStyle'
 import SavedCards from './SavedCards'
 import { LinearGradient } from 'expo-linear-gradient'
+import { useSelector } from 'react-redux'
 
 export default function CardsCheckout() {
+    const { user } = useSelector(state => state.reducer)
+    const [credit_cards, setCreditCards] = useState([])
     const [state, setState] = useState({
         modalVisible: false,
         title: ""
     })
+    useEffect(() => {
+        let componentMount = true
+        if (componentMount) {
+            const { cards } = JSON.parse(user)
+            setCreditCards(cards)
+        }
+        return () => {
+            componentMount = false
+        }
+    }, [user])
+
     return (
         <SafeAreaView style={styles.container}>
             <SavedCards credit_cards={credit_cards} />
