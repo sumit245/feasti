f054ad4e94f777b84fdda67e855a285af6179f85
sumit245 <sumitranjan245@gmail.com>
2022-11-28 01:17:32 +0530

    Manage Cards'

--- src/scenes/Components/managecards/CardsCheckout.js
+++ src/scenes/Components/managecards/CardsCheckout.js
@@ -1,9 +1,11 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { styles } from '../../styles/HomeStyle'
 import SavedCards from './SavedCards'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useSelector } from 'react-redux'
+import HeaderSimple from '../home/headerTop/HeaderSimple'
+import { Provider } from 'react-native-paper'
 
 export default function CardsCheckout() {
     const { user } = useSelector(state => state.reducer)
@@ -24,17 +26,20 @@ export default function CardsCheckout() {
     }, [user])
 
     return (
-        <SafeAreaView style={styles.container}>
-            <SavedCards credit_cards={credit_cards} />
-            <LinearGradient colors={["#ff9900", "#ff6600"]} style={styles.bottomBtnRound}>
-                <TouchableOpacity
-                    onPress={() => {
-                        setState({ modalVisible: true, title: "Add Card" });
-                    }}
-                >
-                    <Text style={[styles.btnText, { marginLeft: 0 }]}>ADD NEW Card</Text>
-                </TouchableOpacity>
-            </LinearGradient>
-        </SafeAreaView>
+        <Provider>
+            <SafeAreaView style={styles.container}>
+                <HeaderSimple navigation={navigation} title="Select Card" />
+                <SavedCards credit_cards={credit_cards} />
+                <LinearGradient colors={["#ff9900", "#ff6600"]} style={styles.bottomBtnRound}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setState({ modalVisible: true, title: "Add Card" });
+                        }}
+                    >
+                        <Text style={[styles.btnText, { marginLeft: 0 }]}>ADD NEW Card</Text>
+                    </TouchableOpacity>
+                </LinearGradient>
+            </SafeAreaView>
+        </Provider>
     )
 }
