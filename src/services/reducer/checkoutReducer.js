8a24f942dc96aa553fe8d616d6920e99917784fd
sumit245 <sumitranjan245@gmail.com>
2022-11-28 23:29:40 +0530

    message

--- src/services/reducer/checkoutReducer.js
+++ src/services/reducer/checkoutReducer.js
@@ -26,7 +26,8 @@ import {
     SET_BASE_PRICE,
     SET_CATEGORY,
     SET_MEAL_TYPE,
-    SET_RESATURANT_ADDRESS
+    SET_RESATURANT_ADDRESS,
+    SET_SERVICE_RATES
 } from "../actions/checkoutAction";
 
 const order = {
@@ -95,6 +96,8 @@ export default function checkoutReducer(state = order, action) {
             return ({ ...state, currentSlots: action.payload })
         case SET_SERVICE_FEE:
             return ({ ...state, serviceFee: action.payload })
+        case SET_SERVICE_RATES:
+            return ({ ...state, service_rate: action.payload })
         case SET_TAXES:
             return ({ ...state, taxes: action.payload })
         default:
