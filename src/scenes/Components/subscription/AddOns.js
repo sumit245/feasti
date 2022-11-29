ca0cbfb95ede9415daf5db400b16817b50d6bb2b
sumit245 <sumitranjan245@gmail.com>
2022-11-30 04:54:41 +0530

    Message

--- src/scenes/Components/subscription/AddOns.js
+++ src/scenes/Components/subscription/AddOns.js
@@ -1,7 +1,20 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function AddOns({ addon }) {
+    useEffect(() => {
+        let componentMounted = true
+        if (componentMounted) {
+            console.log('====================================');
+            console.log(addon);
+            console.log('====================================');
+        }
+
+        return () => {
+            componentMounted = false
+        }
+    }, [])
+
     return (
         <View>
             <Text>AddOns</Text>
