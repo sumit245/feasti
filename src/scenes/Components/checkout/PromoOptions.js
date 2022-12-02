a538d43b39bcd7389b3bc54bc8bf166353f3762e
sumit245 <sumitranjan245@gmail.com>
2022-12-03 04:36:51 +0530

    Message

--- src/scenes/Components/checkout/PromoOptions.js
+++ src/scenes/Components/checkout/PromoOptions.js
@@ -65,11 +65,10 @@ export default function PromoOptions() {
                             {
                                 coupon.isDelivery ?
                                     `Get ${coupon.discount_type === "$"
-                                        ? "$" + coupon.discount : coupon.discount + "%"} off on your delivery.
-                                `:
+                                        ? "$" + coupon.discount : coupon.discount + "%"} off on your delivery.` :
                                     `Get ${coupon.discount_type === "$"
                                         ? "$" + coupon.discount
-                                        : coupon.discount} off on ${coupon.plan_name}.`
+                                        : coupon.discount} off on ${coupon.plan_name || ""}.`
                             }
                             {"\n"}Use Code
                             <Text style={{ fontWeight: "bold", marginLeft: 4 }}>
