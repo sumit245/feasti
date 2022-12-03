8f66af872e6d15467fa6eb6392c76e144c4e7079
sumit245 <sumitranjan245@gmail.com>
2022-12-04 03:49:20 +0530

    Manage Address

--- src/scenes/Components/manageaddress/NoAddress.js
+++ src/scenes/Components/manageaddress/NoAddress.js
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { styles } from '../../styles/HomeStyle'
+import Icon from "react-native-vector-icons/Ionicons"
 
 export default function NoAddress() {
     return (
         <View style={styles.centeredView}>
-            <Icon name="sad" size={80} color="#666" />
+            <Icon name="sad-outline" size={80} color="#666" />
             <Text style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'center' }}>
                 KNOCK KNOCK! WHO'S THERE?
             </Text>
