ae7ffe2bff79e3aacc5176018f1cd4e99e6de63b
sumit245 <sumitranjan245@gmail.com>
2022-12-06 05:08:03 +0530

    Message

--- src/scenes/Components/myorders/OrderCard.js
+++ src/scenes/Components/myorders/OrderCard.js
@@ -46,7 +46,10 @@ export default function OrderCard({ item, navigation }) {
     const openReceipt = async (id) => {
         const response = await axios.get(`${GET_ORDER_DETAILS}${id}`)
         const order = response.data
-        navigation.navigate('order_details', { order, title: order.order_id })
+        console.log('====================================');
+        console.log(order);
+        console.log('====================================');
+        // navigation.navigate('order_details', { order, title: order.order_id })
 
     }
     return (
