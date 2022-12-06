04dfdb2b1b6be1a09ce7c09ec00297a8bae1b244
sumit245 <sumitranjan245@gmail.com>
2022-12-07 02:07:08 +0530

    Message

--- src/scenes/Components/subscription/FutureMeals.js
+++ src/scenes/Components/subscription/FutureMeals.js
@@ -1,30 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View } from 'react-native'
 import { TabBar, TabView } from "react-native-tab-view";
 import CurrentMeal from "./CurrentMeal";
 import { width } from "../../styles/HomeStyle";
+import { set } from "react-native-reanimated";
 export default function FutureMeals({ meals, futuredays }) {
     const [index, setIndex] = useState(0);
+    const [data, setData] = useState([])
 
     const [routes] = useState([
         { key: "first", title: futuredays[0] },
         { key: "second", title: futuredays[1] },
-        { key:"third", title: futuredays[2] }
-        
+        { key: "third", title: futuredays[2] }
+
     ]);
+
+    useEffect(() => {
+        setData(meals)
+    }, [meals])
+
 
     const renderTabBar = (props) => (
         <TabBar
             {...props}
-            tabStyle={{ width: width / 3.3,flexDirection:'row' }}
+            tabStyle={{ width: width / 3.3, flexDirection: 'row' }}
             scrollEnabled
             style={{
                 backgroundColor: "transparent",
-             //   flexDirection: 'row',
-              
+                //   flexDirection: 'row',
+
             }}
-            indicatorContainerStyle={{flexDirection:'row'}}
-            contentContainerStyle={{ flexDirection: 'row', justifyContent: 'space-between',flex:1 }}
+            indicatorContainerStyle={{ flexDirection: 'row' }}
+            contentContainerStyle={{ flexDirection: 'row', justifyContent: 'space-between', flex: 1 }}
             activeColor="#ff6600"
             labelStyle={{ fontWeight: "bold" }}
             inactiveColor="#272727"
@@ -35,11 +42,11 @@ export default function FutureMeals({ meals, futuredays }) {
     const renderScene = ({ route }) => {
         switch (route.key) {
             case "first":
-                return <CurrentMeal index={route.key} meal={meals.find((o) => o.day === route.title)} />;
+                return <CurrentMeal index={route.key} meal={data.find((o) => o.day === route.title)} />;
             case "second":
-                return <CurrentMeal index={route.key} meal={meals.find((o) => o.day === route.title)} />;
+                return <CurrentMeal index={route.key} meal={data.find((o) => o.day === route.title)} />;
             case "third":
-                return <CurrentMeal index={route.key} meal={meals.find((o) => o.day === route.title)} />;
+                return <CurrentMeal index={route.key} meal={data.find((o) => o.day === route.title)} />;
             default:
                 break;
         }
