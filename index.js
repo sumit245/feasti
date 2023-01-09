9e6635e202d41f0a31c552db5d2bf1b66feececb
sumit245 <sumitranjan245@gmail.com>
2023-01-10 01:38:46 +0530

    Local

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
+import { Platform,AppRegistry } from 'react-native';
 import App from './App';
+import {name as appName} from './app.json'
 
-registerRootComponent(App);
+if (Platform.OS === "android") {
+    registerRootComponent(App);
+} else {
+    AppRegistry.registerComponent(appName, () => App);
+}
